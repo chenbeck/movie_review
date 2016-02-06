96859c43d0352fc86338d73711765b009ccef7dd
Beck Chen <chenbeck@gmail.com>
2016-02-06 14:18:17 +0800

    movie & review association setup

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,7 @@
 class ReviewsController < ApplicationController
   before_action :set_review, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
+  before_action :set_movie
 
   def index
     @reviews = Review.all
@@ -19,9 +20,10 @@ class ReviewsController < ApplicationController
   def create
     @review = Review.new(review_params)
     @review.user_id = current_user.id
+    @review.movie_id = @movie.id
 
     if @review.save
-      redirect_to @review
+      redirect_to @movie
     else
       render 'new'
     end
@@ -47,6 +49,10 @@ class ReviewsController < ApplicationController
   private
     def set_review
       @review = Review.find(params[:id])
+    end
+
+    def set_movie
+      @movie = Movie.find(params[:movie_id])
     end
 
     def review_params
